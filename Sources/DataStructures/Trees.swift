e28000bbd1989c2d22efeeec75c10e6b8bfacbda
Louis DeScioli <louis.descioli@gmail.com>
2020-08-29 16:35:58 -0400

    Implements some solutions

--- Sources/DataStructures/Trees.swift
+++ Sources/DataStructures/Trees.swift
@@ -32,7 +32,23 @@ extension Collection where Indices.Iterator.Element == Index {
 //
 
 func isBinaryTree<T>(_ tree: Tree<T>) -> Bool {
-  return false
+  func isBinaryTree<T>(_ node: Node<T>?) -> Bool {
+    guard let node = node else {
+      return true
+    }
+    if node.children.count > 2 {
+      return false
+    }
+    for child in node.children {
+      if !isBinaryTree(child) {
+        return false
+      }
+    }
+    return true
+    // alternative to for loop, a little more functional
+    // return node.children.reduce(true, { result, child in result && isBinaryTree(child) })
+  }
+  return isBinaryTree(tree.root)
 }
 
 func isBinarySearchTree<T>(_ tree: Tree<T>) -> Bool {
@@ -48,19 +64,62 @@ func balance<T>(_ tree: Tree<T>) {
 }
 
 func breadthFirstTraversal<T>(_ tree: Tree<T>, _ visit: ((T) -> Void)) {
+  guard let root = tree.root else {
+    return
+  }
+  visit(root.value)
+  var queue = root.children
+  while !queue.isEmpty {
+    let node = queue.removeFirst()
+    visit(node.value)
+    queue.append(contentsOf: node.children)
+  }
 
+  // alternative
+  // var i = 0
+  // while i < queue.count {
+  //   let node = queue[i]
+  //   visit(node.value)
+  //   queue.append(contentsOf: node.children)
+  //   i += 1
+  // }
 }
 
 func depthFirstPreOrderTraversal<T>(_ tree: Tree<T>, _ visit: ((T) -> Void)) {
-
+  func depthFirstPreOrderTraversal<T>(_ node: Node<T>?, _ visit: ((T) -> Void)) {
+    guard let node = node else {
+      return
+    }
+    visit(node.value)
+    for child in node.children {
+      depthFirstPreOrderTraversal(child, visit)
+    }
+  }
+  depthFirstPreOrderTraversal(tree.root!, visit)
 }
 
 func depthFirstInOrderTraversal<T>(_ tree: Tree<T>, _ visit: ((T) -> Void)) {
-
+  func depthFirstInOrderTraversal<T>(_ node: Node<T>?, _ visit: ((T) -> Void)) {
+    guard let node = node else {
+      return
+    }
+    depthFirstInOrderTraversal(node.children[safe: 0], visit)
+    visit(node.value)
+    depthFirstInOrderTraversal(node.children[safe: 1], visit)
+  }
+  depthFirstInOrderTraversal(tree.root, visit)
 }
 
 func depthFirstPostOrderTraversal<T>(_ tree: Tree<T>, _ visit: ((T) -> Void)) {
-
+  func depthFirstPostOrderTraversal<T>(_ node: Node<T>?, _ visit: ((T) -> Void)) {
+    guard let node = node else {
+      return
+    }
+    depthFirstPostOrderTraversal(node.children[safe: 0], visit)
+    depthFirstPostOrderTraversal(node.children[safe: 1], visit)
+    visit(node.value)
+  }
+  depthFirstPostOrderTraversal(tree.root, visit)
 }
 
 func prettyTreeString<T>(_ tree: Tree<T>) -> String {
