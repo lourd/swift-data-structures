cb6980be6a9bac9b0e8736a52bd4a7ab1d0b17a4
Louis DeScioli <louis.descioli@gmail.com>
2020-08-31 23:22:33 -0400

    Implements tree balancing solution

--- Sources/DataStructures/Trees.swift
+++ Sources/DataStructures/Trees.swift
@@ -1,3 +1,5 @@
+import Foundation
+
 //
 //  DATA STRUCTURES
 //
@@ -43,7 +45,7 @@ class Tree<T: Comparable>: Equatable {
 }
 
 //
-// ALGORITHMS
+// TREE CLASSIFYING ALGORITHMS
 //
 
 func isBinaryTree<T>(_ tree: Tree<T>) -> Bool {
@@ -54,7 +56,7 @@ func isBinaryTree<T>(_ tree: Tree<T>) -> Bool {
     if node.children.count > 2 {
       return false
     }
-    for child in node.children {
+    for (_, child) in node.children {
       if !isBinaryTree(child) {
         return false
       }
@@ -77,13 +79,13 @@ func isBinarySearchTree<T>(_ tree: Tree<T>) -> Bool {
     if node.children.count > 2 {
       return false
     }
-    let leftChild = node.children[safe: 0]
+    let leftChild = node.leftChild
     if let leftChild = leftChild {
       if !(leftChild.value < node.value) {
         return false
       }
     }
-    let rightChild = node.children[safe: 1]
+    let rightChild = node.rightChild
     if let rightChild = rightChild {
       if !(rightChild.value > node.value) {
         return false
@@ -95,8 +97,20 @@ func isBinarySearchTree<T>(_ tree: Tree<T>) -> Bool {
 }
 
 func isBalancedBinarySearchTree<T>(_ tree: Tree<T>) -> Bool {
-  return false
+  guard isBinarySearchTree(tree) else {
+    return false
+  }
+  guard let root = tree.root else {
+    return false
+  }
+  let leftHeight = getHeight(root.leftChild)
+  let rightHeight = getHeight(root.rightChild)
+  return abs(leftHeight - rightHeight) <= 1
 }
+
+//
+//  TREE DATA COMPUTING FUNCTIONS
+//
 
 func getHeight<T>(_ tree: Tree<T>) -> Int {
   return getHeight(tree.root)
@@ -106,24 +120,39 @@ func getHeight<T>(_ node: Node<T>?) -> Int {
   guard let node = node else {
     return 0
   }
-  let maxChildHeight = node.children.map(getHeight).max() ?? 0
+  let maxChildHeight = node.children.compactMap { getHeight($1) }.max() ?? 0
   return 1 + maxChildHeight
 }
 
-func balance<T>(_ tree: Tree<T>) {
-
+func getNodeCount<T>(_ tree: Tree<T>) -> Int {
+  return getNodeCount(tree.root)
 }
+
+func getNodeCount<T>(_ node: Node<T>?) -> Int {
+  guard let node = node else {
+    return 0
+  }
+  return node.children.reduce(1) { count, item in count + getNodeCount(item.value) }
+}
+
+func getBalanceFactor<T>(_ node: Node<T>) -> Int {
+  return getHeight(node.rightChild) - getHeight(node.leftChild)
+}
+
+//
+// TRAVERSAL FUNCTIONS
+//
 
 func breadthFirstTraversal<T>(_ tree: Tree<T>, _ visit: ((T) -> Void)) {
   guard let root = tree.root else {
     return
   }
   visit(root.value)
-  var queue = root.children
+  var queue = root.children.sorted(by: { $0.key < $1.key })
   while !queue.isEmpty {
-    let node = queue.removeFirst()
+    let (_, node) = queue.removeFirst()
     visit(node.value)
-    queue.append(contentsOf: node.children)
+    queue.append(contentsOf: node.children.sorted(by: { $0.key < $1.key }))
   }
 
   // alternative
@@ -142,7 +171,7 @@ func depthFirstPreOrderTraversal<T>(_ tree: Tree<T>, _ visit: ((T) -> Void)) {
       return
     }
     visit(node.value)
-    for child in node.children {
+    for (_, child) in node.children.sorted(by: { $0.key < $1.key }) {
       depthFirstPreOrderTraversal(child, visit)
     }
   }
@@ -154,9 +183,9 @@ func depthFirstInOrderTraversal<T>(_ tree: Tree<T>, _ visit: ((T) -> Void)) {
     guard let node = node else {
       return
     }
-    depthFirstInOrderTraversal(node.children[safe: 0], visit)
+    depthFirstInOrderTraversal(node.leftChild, visit)
     visit(node.value)
-    depthFirstInOrderTraversal(node.children[safe: 1], visit)
+    depthFirstInOrderTraversal(node.rightChild, visit)
   }
   depthFirstInOrderTraversal(tree.root, visit)
 }
@@ -166,8 +195,8 @@ func depthFirstPostOrderTraversal<T>(_ tree: Tree<T>, _ visit: ((T) -> Void)) {
     guard let node = node else {
       return
     }
-    depthFirstPostOrderTraversal(node.children[safe: 0], visit)
-    depthFirstPostOrderTraversal(node.children[safe: 1], visit)
+    depthFirstPostOrderTraversal(node.leftChild, visit)
+    depthFirstPostOrderTraversal(node.rightChild, visit)
     visit(node.value)
   }
   depthFirstPostOrderTraversal(tree.root, visit)
@@ -175,4 +204,142 @@ func depthFirstPostOrderTraversal<T>(_ tree: Tree<T>, _ visit: ((T) -> Void)) {
 
 func prettyTreeString<T>(_ tree: Tree<T>) -> String {
   return ""
+}
+
+//
+//  TREE BALANCING & MANIPULATION FUNCTIONS
+//
+
+func rotateLeft<T>(_ node: Node<T>) -> Node<T> {
+  guard let newParent = node.rightChild else {
+    return node
+  }
+  node.rightChild = newParent.leftChild
+  newParent.leftChild = node
+  return newParent
+}
+
+func rotateRight<T>(_ node: Node<T>) -> Node<T> {
+  guard let newParent = node.leftChild else {
+    return node
+  }
+  node.leftChild = newParent.rightChild
+  newParent.rightChild = node
+  return newParent
+}
+
+@discardableResult func balanceBinarySearchTree<T>(_ tree: Tree<T>) -> Tree<T> {
+  guard let root = tree.root else {
+    return tree
+  }
+  guard isBinarySearchTree(tree) else {
+    return tree
+  }
+  func oneOffBalance<T>(_ node: Node<T>) -> Node<T> {
+    let balanceFactor = getBalanceFactor(node)
+    guard abs(balanceFactor) == 2 else {
+      return node
+    }
+    if balanceFactor == -2 {
+      let childBalanceFactor = getBalanceFactor(node.leftChild!)
+      if childBalanceFactor > 0 {
+        node.leftChild = rotateLeft(node.leftChild!)
+      }
+      return rotateRight(node)
+    } else if balanceFactor == 2 {
+      let childBalanceFactor = getBalanceFactor(node.rightChild!)
+      if childBalanceFactor < 0 {
+        node.rightChild = rotateRight(node.rightChild!)
+      }
+      return rotateLeft(node)
+    }
+    return node
+  }
+  ///
+  /// The Day–Stout–Warren algorithm
+  ///
+  func fullRebalance<T>(_ tree: Tree<T>) {
+    treeToVine(tree)
+    vineToTree(tree)
+  }
+  let balanceFactor = abs(getBalanceFactor(root))
+  guard balanceFactor > 1 else {
+    // Already balanced
+    return tree
+  }
+  guard balanceFactor > 2 else {
+    // Only off balance by insertion, do some rotations to re-balance
+    tree.root = oneOffBalance(root)
+    return tree
+  }
+  // Off balance by any number, do a full tree re-shaping
+  fullRebalance(tree)
+  return tree
+}
+
+/// Turns the tree into a right child "vine"
+@discardableResult func treeToVine<T>(_ tree: Tree<T>) -> Tree<T> {
+  // Recursive version
+  func treeToVine<T>(_ node: Node<T>?) -> Node<T>? {
+    // Base case - the node is nil
+    guard let parent = node else {
+      return node
+    }
+    if parent.leftChild == nil {
+      parent.rightChild = treeToVine(parent.rightChild)
+      return parent
+    }
+    let newParent = rotateRight(parent)
+    return treeToVine(newParent)
+  }
+  tree.root = treeToVine(tree.root)
+
+  // Iterative version
+  // var grandParent: Node<T>?
+  // var parent = tree.root
+  // while parent != nil {
+  //   if parent!.leftChild == nil {
+  //     grandParent = parent
+  //     parent = parent!.rightChild
+  //     continue
+  //   }
+  //   let newParent = rotateRight(parent!)
+  //   if parent === tree.root {
+  //     tree.root = newParent
+  //   } else {
+  //     grandParent!.rightChild = newParent
+  //   }
+  //   parent = newParent
+  // }
+
+  return tree
+}
+
+@discardableResult func vineToTree<T>(_ tree: Tree<T>) -> Tree<T> {
+  let nodeCount = getNodeCount(tree)
+  guard var root = tree.root, nodeCount > 2 else {
+    return tree
+  }
+  var bottomRowLeaves = nodeCount + 1 - Int(log2(Float(nodeCount + 1)).rounded(.up))
+  func makeRotations<T>(_ root: Node<T>, _ rotations: Int) -> Node<T> {
+    var parent = root
+    var newRoot: Node<T>?
+    for i in 0..<rotations {
+      if i == 0 {
+        parent = rotateLeft(parent)
+        newRoot = parent
+      } else {
+        parent.rightChild = rotateLeft(parent.rightChild!)
+        parent = parent.rightChild!
+      }
+    }
+    return newRoot!
+  }
+  root = makeRotations(root, nodeCount - bottomRowLeaves)
+  while bottomRowLeaves > 2 {
+    bottomRowLeaves /= 2
+    root = makeRotations(root, bottomRowLeaves)
+  }
+  tree.root = root
+  return tree
 }
